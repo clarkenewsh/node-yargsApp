9e21763d56b23609a95e308e31be6f53985bbd91
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-22 10:13:13 +0000

    yargs app - activity 2 completed - add mutiple movies

--- src/app.js
+++ src/app.js
@@ -11,11 +11,20 @@ const app = (yargsObj) => {
         movie.add();
         console.log(movie.list());
         // Add movie to array, console log at end of runtime
+    } else if(yargsObj.addMore) {
+        const movie1 = new Movie(yargsObj.title, yargsObj.actor);
+        const movie2 = new Movie(yargsObj.title1, yargsObj.actor1);
+        movie1.add();
+        // console.log(movie1.list());
+        movie2.add();
+        console.log(movie1.list());
+
     } else {
         console.log("incorrect command");
     }
 }
 
+// Call the app function
 app(yargs.argv);
 // console.log(yargs.argv.name);
 
